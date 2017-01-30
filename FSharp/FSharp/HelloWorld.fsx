300a90069f4893bc24f75c614a80fad37da6de3c
Michael Naumenko <agaspher@outlook.com>
2017-01-30 08:47:56 +0300

    Try function composition and back pipe operators

--- FSharp/FSharp/HelloWorld.fsx
+++ FSharp/FSharp/HelloWorld.fsx
@@ -13,3 +13,16 @@ let testL() = "hello"
 let testM x = x=x
 let testN x = x 1          // hint: what kind of thing is x?
 let testO x:string = x 1   // hint: what does :string modify? 
+
+let add1 = (+) 1
+let toString x = x.ToString()
+let myFunc x y =
+    printfn "X is %s" x
+    printfn "Y is %s" y
+
+3 |> add1 |> toString |> myFunc <| (4 |> add1 |> toString)
+
+let add n x = x + n
+let add1ThenMultiply = (+) 1 >> (*) 
+let times n x = x * n
+let times2Add1 = add 1 << times 2
