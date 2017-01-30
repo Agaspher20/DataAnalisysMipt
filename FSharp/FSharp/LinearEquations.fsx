aa89c3cc817f3d7a19c9b66df9183059fb85463e
Michael Naumenko <agaspher@outlook.com>
2017-01-30 08:08:56 +0300

    Week2 programming assignment on f#

---
+++ FSharp/FSharp/LinearEquations.fsx
@@ -0,0 +1,37 @@
+﻿#I @"..\packages\"
+#r @"MathNet.Numerics.Signed.3.17.0\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.Signed.3.17.0\lib\net40\MathNet.Numerics.FSharp.dll"
+
+open System
+open System.IO
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+
+let resultPath = __SOURCE_DIRECTORY__ + @"..\..\..\Results\submission-2.txt"
+
+let func (x:float) =
+    Math.Sin(x/5.) * Math.Exp(x/10.) + 5. * Math.Exp(-x/2.)
+    
+let buildEquation (size:int) (points:List<float>) =
+    let equationMatrix =
+        points
+        |> List.map (fun point ->
+            [0..size] |> List.map (fun pow -> Math.Pow(point, float pow)))
+    let equationVector = points |> List.map func
+    (matrix equationMatrix, vector equationVector)
+
+let buildResult = Seq.map (fun r -> r.ToString()) >> String.concat " "
+
+let a1,b1 = [1.;15.] |> buildEquation 1
+a1.Solve b1 |> buildResult |> printfn "%s"
+
+let a2,b2 = [1.;8.;15.] |> buildEquation 2
+a2.Solve b2 |> buildResult |> printfn "%s"
+
+let a3,b3 = [1.;4.;10.;15.] |> buildEquation 3
+let result = a3.Solve b3 |> buildResult
+result |> printfn "%s"
+
+let resultWriter = File.CreateText resultPath
+result |> resultWriter.Write
+resultWriter.Close()
---
+++ FSharp/FSharp/StringsComparison.fsx
@@ -0,0 +1,66 @@
+﻿#I @"..\packages\"
+#r @"MathNet.Numerics.Signed.3.17.0\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.Signed.3.17.0\lib\net40\MathNet.Numerics.FSharp.dll"
+
+open System.IO
+open System.Text.RegularExpressions
+open MathNet.Numerics
+
+let filePath = __SOURCE_DIRECTORY__ + @"..\..\..\Data\sentences.txt"
+let resultPath = __SOURCE_DIRECTORY__ + @"..\..\..\Results\submission-1.txt"
+
+let cosine (line0:float[]) (line1:float[]) = Distance.Cosine(line0, line1)
+
+let tokenize (line:string) =
+    line.ToLowerInvariant()
+    |> Regex(@"[a-z]+").Matches
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> m.Value)
+    |> Seq.countBy(fun token -> token)
+
+let lines = 
+    filePath
+    |> File.ReadLines
+    |> Seq.map (fun line -> (line, line |> tokenize))
+    |> Seq.toArray
+
+let allTokens =
+    lines
+    |> Seq.collect (fun (_,tokens) -> tokens |> Seq.map (fun (key,_) -> key))
+    |> Set.ofSeq
+
+let tokensByLines =
+    lines
+    |> Seq.map (fun (_,tokens) ->
+        let tokensMap = tokens |> Map.ofSeq
+        allTokens
+        |> Seq.map (fun token ->
+            match tokensMap |> Map.tryFind token with
+            | None -> 0.
+            | Some count -> (float count))
+        |> Seq.toArray)
+    |> Seq.toList
+
+let firstLine = tokensByLines.[0]
+let distanceToFirstLine = cosine firstLine
+
+let results =
+    tokensByLines.[1..]
+    |> List.mapi (fun index vector -> (index+1, vector |> distanceToFirstLine))
+    |> List.sortBy (fun (_,distance) -> distance)
+    |> List.take 2
+let result = results |> List.map (fun (index,_) -> index.ToString()) |> String.concat " "
+
+printfn "Matrix shape: %i %i" (tokensByLines |> List.length) (firstLine |> Array.length)
+printfn "First sentence: \"%s\"\n" (fst lines.[0])
+
+results
+|> List.iter (fun (index, distance) ->
+    printfn "Distance: %f Index: %i" distance index
+    printfn "%s\n" (lines.[index] |> fst))
+
+printfn "Result: %s" result
+
+let resultWriter = resultPath |> File.CreateText
+result |> resultWriter.Write
+resultWriter.Close()
