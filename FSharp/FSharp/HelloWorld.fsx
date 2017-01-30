24d252a34f9af0a107d95fcc0ac305d338f0d31a
Michael Naumenko <agaspher@outlook.com>
2017-01-30 08:07:56 +0300

    F# Hello world

---
+++ FSharp/FSharp/HelloWorld.fsx
@@ -0,0 +1,15 @@
+﻿let testA   = float 2
+let testB x = float 2
+let testC x = float 2 + x
+let testD x = x.ToString().Length
+let testE (x:float) = x.ToString().Length
+let testF x = printfn "%s" x
+let testG x = printfn "%f" x
+let testH   = 2 * 2 |> ignore
+let testI x = 2 * 2 |> ignore
+let testJ (x:int) = 2 * 2 |> ignore
+let testK   = "hello"
+let testL() = "hello"
+let testM x = x=x
+let testN x = x 1          // hint: what kind of thing is x?
+let testO x:string = x 1   // hint: what does :string modify? 
