bf500b810b8e0fc4a26ec2e97dc82a7328a48aa3
Michael Naumenko <agaspher@outlook.com>
2017-02-16 17:31:51 +0300

    Fixed indentation

--- FSharp/FSharp/CentralLimitTheorem.fsx
+++ FSharp/FSharp/CentralLimitTheorem.fsx
@@ -8,34 +8,53 @@ open MathNet.Numerics.Distributions
 open MathNet.Numerics.Statistics
 open FSharp.Charting
 
-let calcMean (data:seq<float>) = MathNet.Numerics.Statistics.Statistics.Mean(data)
-let line (min:float) (max:float) (data:seq<float*float>) = Chart.Line(data).WithXAxis(Min=min,Max=max)
-let histogram (min:float) (max:float) (data:seq<float>) = Chart.Histogram(data, LowerBound = min, UpperBound=max)
+let calcMean (data:seq<float>) = Statistics.Mean(data)
+let line (min:float)
+         (max:float)
+         (data:seq<float*float>) =
+    Chart.Line(data).WithXAxis(Min=min,Max=max)
+let histogram (min:float)
+              (max:float)
+              (data:seq<float>) =
+    Chart.Histogram(data, LowerBound = min, UpperBound=max)
 let sqrt x = System.Math.Sqrt x
 
 let min = 0.
 let max = 1.
-let arcSinDistrib = MathNet.Numerics.Distributions.Beta(0.5, 0.5)
+let arcSinDistrib = Beta(0.5, 0.5)
 let hist (data:seq<float>) = histogram min max data
 let lin (data:seq<float*float>) = line min max data
     
-let samples = arcSinDistrib.Samples() |> Seq.take 1000
-let pdf = seq{ 0.001 .. 0.001 .. 0.999 } |> Seq.map(fun n -> n,arcSinDistrib.Density(n))
+let samples = arcSinDistrib.Samples()
+              |> Seq.take 1000
+let pdf = seq{ 0.001 .. 0.001 .. 0.999 }
+          |> Seq.map(fun n -> n,arcSinDistrib.Density(n))
 Chart.Rows([hist samples; lin pdf])
 
-let buildRange (distribution:IContinuousDistribution) (size:int) (rangeSize:int) =
+let buildRange (distribution:IContinuousDistribution)
+               (size:int)
+               (rangeSize:int) =
     seq { 0 .. rangeSize }
-    |> Seq.map (fun i -> distribution.Samples() |> Seq.take size |> calcMean)
+    |> Seq.map (fun i ->
+        distribution.Samples()
+        |> Seq.take size
+        |> calcMean)
 
-let buildNormalDistribution (mean:float) (variance:float) (min:float) (max:float) (size:int) (step:float) =
-    let n = MathNet.Numerics.Distributions.Normal(mean, System.Math.Sqrt(variance/(float size)))
+let buildNormalDistribution (mean:float)
+                            (variance:float)
+                            (min:float)
+                            (max:float)
+                            (size:int)
+                            (step:float) =
+    let n = Normal(mean, Math.Sqrt(variance/(float size)))
     seq { min .. step .. max }
     |> Seq.map (fun x -> x,n.Density(x))
 
 let variance = 1./8. // variance
 let mean = 0.5 // mean
 let assembleData (size:int) =
-    buildRange arcSinDistrib size 1000,buildNormalDistribution mean variance 0. 1. size 0.001
+    buildRange arcSinDistrib size 1000,
+    buildNormalDistribution mean variance 0. 1. size 0.001
 
 let fiveRange,fiveNormal = assembleData 5
 Chart.Rows([ hist fiveRange; lin fiveNormal ])
