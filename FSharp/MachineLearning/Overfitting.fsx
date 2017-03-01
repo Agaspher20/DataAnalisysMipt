8a93017554704e4ecc60aa991e5e29fc672610cb
Michael Naumenko <agaspher@outlook.com>
2017-03-01 20:31:57 +0300

    implemented item from cnt dependency plot

--- FSharp/MachineLearning/Overfitting.fsx
+++ FSharp/MachineLearning/Overfitting.fsx
@@ -1,6 +1,7 @@
 #r @"..\packages\MathNet.Numerics.Signed.3.17.0\lib\net40\MathNet.Numerics.dll"
 #r @"..\packages\MathNet.Numerics.FSharp.Signed.3.17.0\lib\net40\MathNet.Numerics.FSharp.dll"
 #r @"..\packages\FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+#r @"..\packages\FSharp.Charting.0.90.14\lib\net40\FSharp.Charting.dll"
 #r @"bin\Debug\LibExtensions.dll"
 
 
@@ -8,8 +9,10 @@ open System
 open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 
-open LibExtensions.Charting
 open FSharp.Data
+open FSharp.Charting
+
+open LibExtensions.Charting
 
 [<Literal>]
 let DataPath = __SOURCE_DIRECTORY__ + @"..\..\..\Data\bikes_rent.csv"
@@ -18,8 +21,8 @@ type Observation = Data.Row
 
 let dataSet = Data.Load(DataPath)
 let data = dataSet.Rows
-let obsToList (obs:Observation) =
-    [
+let obsToArray (obs:Observation) =
+    [|
         obs.Season |> float;
         obs.Yr |> float;
         obs.Mnth |> float;
@@ -33,7 +36,7 @@ let obsToList (obs:Observation) =
         obs.``Windspeed(mph)``;
         obs.``Windspeed(ms)``;
         obs.Cnt |> float
-    ]
+    |]
 
 let formatObservation (obs:Observation) =
     sprintf "%6i %2i %4i %7i %7i %10i %10i %4.1f %5.2f %3.2f %12f %13f %3i"
@@ -59,21 +62,25 @@ data
 |> Seq.take 5
 |> Seq.iter (formatObservation >> (printfn "%s"))
 
-let chartData = data |> Seq.map (fun obs -> (obs |> obsToList))
+let counts = data |> Seq.map(fun obs -> obs.Cnt |> float) |> Seq.toList
+let pairPlotData = data |> Seq.map obsToArray |> Seq.toList
+
 headers.[0..headers.Length-2]
-    |> Array.mapi (fun i h ->
-        let cd = chartData |> Seq.map (fun rl ->rl.[i],rl|>List.last)
-        i/2,(h,cd))
-    |> Array.groupBy (fun (k,_) -> k)
-    |> Array.map(fun (_,c) ->
-        let keys,charts = c |> Array.unzip
-        Chart.Rows(
-            charts
-            |> Seq.map(fun (h,c) ->
-                Chart.Point(c,
-                            Name=sprintf "Count from %s dependency" h,
-                            Title=sprintf "Count(%s)" h,
-                            XTitle=h,
-                            YTitle="Count",
-                            Color=Drawing.Color.Red,
-                            MarkerColor=Drawing.Color.Green))))
+|> Array.mapi (fun i h -> i,h,(pairPlotData|>Seq.map(fun a -> a.[i])))
+|> Array.groupBy (fun (idx,_,_) -> idx/4)
+|> Array.map (fun (_,columns) ->
+    columns
+    |> Array.mapi (fun idx (i,title,column) ->
+        let ch = (column,counts)
+                 ||> Seq.zip
+                 |> Chart.Point
+                 |> titleX title
+                 |> hideXLabels
+                 |> hideYLabels
+        match idx with
+        | 0 -> ch |> titleY "Cnt"
+        | _ -> ch
+    )
+    |> Chart.Columns)
+|> Chart.Rows
+|> Chart.Show
