5c6e284afc02f4812fa2cb8b54ae067a1ddd0b4d
Michael Naumenko <agaspher@outlook.com>
2017-06-01 15:37:54 +0300

    Quiz 1_3 task 8 fixed

--- Python/4. StatsDataAnalysis/task1_3.py
+++ Python/4. StatsDataAnalysis/task1_3.py
@@ -2,39 +2,40 @@
 import numpy as np
 from scipy.stats import norm
 #%%
-print np.round(norm.ppf(0.003/2), 4)
+print "3 sigma rule refined: %.4f" % np.round(norm.ppf(0.003/2), 4)
 #%%
-aspirin_n = 11037.
-placebo_n = 11034.
-aspirin_infarct_n = 104.
-placebo_infarct_n = 189.
+aspirin_n = 11037
+placebo_n = 11034
+aspirin_infarct_n = 104
+placebo_infarct_n = 189
 
-aspirin_p = aspirin_infarct_n/aspirin_n
-placebo_p = placebo_infarct_n/placebo_n
+aspirin_p = float(aspirin_infarct_n)/aspirin_n
+placebo_p = float(placebo_infarct_n)/placebo_n
 
-print aspirin_p, placebo_p
-print np.round(placebo_p-aspirin_p, 4)
+print "Aspirin infarct probability: %.4f" % aspirin_p
+print "Placebo infarct probability: %.4f" % placebo_p
+print "Probability diff %.4f" % np.round(placebo_p-aspirin_p, 4)
 
 #%%
 from statsmodels.stats.proportion import proportion_confint
 conf_interval_aspirin = proportion_confint(aspirin_infarct_n, aspirin_n, method="wilson")
 conf_interval_placebo = proportion_confint(placebo_infarct_n, placebo_n, method="wilson")
 
-print "interval for aspirin infarct [%f, %f]" % (np.round(conf_interval_aspirin[0], 4), np.round(conf_interval_aspirin[1], 4))
-print "interval for placebo infarct [%f, %f]" % (np.round(conf_interval_placebo[0], 4), np.round(conf_interval_placebo[1], 4))
+print "interval for aspirin infarct [%.4f, %.4f]" % (np.round(conf_interval_aspirin[0], 4), np.round(conf_interval_aspirin[1], 4))
+print "interval for placebo infarct [%.4f, %.4f]" % (np.round(conf_interval_placebo[0], 4), np.round(conf_interval_placebo[1], 4))
 
 #%%
 def proportions_confint_diff_ind(p1, count1, p2, count2, alpha = 0.05):    
     z = norm.ppf(1 - alpha / 2.)
     
-    left_boundary = (p1 - p2) - z * np.sqrt(p1 * (1 - p1)/ count1 + p2 * (1 - p2)/ count2)
-    right_boundary = (p1 - p2) + z * np.sqrt(p1 * (1 - p1)/ count1 + p2 * (1 - p2)/ count2)
+    left_boundary = (p1 - p2) - z * np.sqrt(p1 * (1. - p1)/ count1 + p2 * (1 - p2)/ count2)
+    right_boundary = (p1 - p2) + z * np.sqrt(p1 * (1. - p1)/ count1 + p2 * (1 - p2)/ count2)
     
     return (left_boundary, right_boundary)
 
 #%%
 conf_interval_diff = proportions_confint_diff_ind(placebo_p, placebo_n, aspirin_p, aspirin_n)
-print "confidence interval: [%f, %f]" % (np.round(conf_interval_diff[0],4),np.round(conf_interval_diff[1],4))
+print "interval for aspirin and placebo probability diff: [%.4f, %.4f]" % (np.round(conf_interval_diff[0],4),np.round(conf_interval_diff[1],4))
 
 #%%
 def calculate_odds(success_count, full_count):
@@ -42,15 +43,15 @@ def calculate_odds(success_count, full_count):
 #%%
 aspirin_odds = calculate_odds(aspirin_infarct_n, aspirin_n)
 placebo_odds = calculate_odds(placebo_infarct_n, placebo_n)
-print np.round(placebo_odds/aspirin_odds, 4)
+print "Placebo to aspirin odds ratio: %.4f" % np.round(placebo_odds/aspirin_odds, 4)
 
 #%%
 aspirin_vector = np.append(
-    np.ones(int(aspirin_infarct_n), dtype=float),
-    np.zeros(int(aspirin_n-aspirin_infarct_n), dtype=float))
+    np.ones(int(aspirin_infarct_n), dtype=int),
+    np.zeros(int(aspirin_n-aspirin_infarct_n), dtype=int))
 placebo_vector = np.append(
-    np.ones(int(placebo_infarct_n), dtype=float),
-    np.zeros(int(placebo_n-placebo_infarct_n), dtype=float)
+    np.ones(int(placebo_infarct_n), dtype=int),
+    np.zeros(int(placebo_n-placebo_infarct_n), dtype=int)
 )
 #%%
 def get_bootstrap_samples(data, n_samples):
@@ -71,12 +72,9 @@ all_aspirin_odds = map(calculate_odds_from_samples, aspirin_samples)
 all_placebo_odds = map(calculate_odds_from_samples, placebo_samples)
 
 #%%
-from statsmodels.stats.weightstats import _tconfint_generic
 def stat_intervals(stat, alpha):
-    mean = np.mean(stat)
-    std = np.std(stat)
-    count = len(stat)
-    return _tconfint_generic(mean, std/np.sqrt(count), count-1, alpha, "two-sided")
+    boundaries = np.percentile(stat, [100 * alpha / 2., 100 * (1 - alpha / 2.)])
+    return boundaries
 #%%
 odds = map(
     lambda (aspirin_odd, placebo_odd): placebo_odd/aspirin_odd,
