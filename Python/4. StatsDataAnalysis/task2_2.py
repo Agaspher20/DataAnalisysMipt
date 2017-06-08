a3af66b5014a7ae3084613303e9631341cc88b7f
Michael Naumenko <agaspher@outlook.com>
2017-06-08 16:02:14 +0300

    task 2_2. Answers 4,5,6,7 not solved

--- Python/4. StatsDataAnalysis/task2_2.py
+++ Python/4. StatsDataAnalysis/task2_2.py
@@ -29,6 +29,8 @@ ctrl_vector = np.append(
     np.zeros(int(n_ctrl-n_ctrl_zev), dtype=int)
 )
 
+print "Difference: %.4f" % np.round(float(n_zev_zev)/float(n_zev) - float(n_ctrl_zev)/float(n_ctrl), 4)
+
 #%%
 conf_interval_zev = proportion_confint(
     sum(zev_vector),
@@ -83,4 +85,91 @@ print "95%% confidence interval for a difference between proportions: [%f, %f]" 
       proportions_diff_confint_ind(zev_vector, ctrl_vector)
 
 #%%
-print "p-value: %f" % proportions_diff_z_test(proportions_diff_z_stat_ind(zev_vector, ctrl_vector))
+pvalue = proportions_diff_z_test(proportions_diff_z_stat_ind(zev_vector, ctrl_vector), alternative='greater')
+print "p-value: %.4f" % np.round(pvalue, 4)
+
+# Имеются данные измерений двухсот швейцарских тысячефранковых банкнот, бывших в обращении в первой половине XX века.
+# Сто из банкнот были настоящими, и сто — поддельными. На рисунке ниже показаны измеренные признаки:
+# Загрузите данные.
+#%%
+import pandas as pd
+frame = pd.read_csv("..\..\Data\\banknotes.txt", sep="\t", header=0)
+frame.head()
+
+# Отделите 50 случайных наблюдений в тестовую выборку с помощью функции sklearn.cross_validation.train_test_split (зафиксируйте random state = 1).
+#%%
+from sklearn.model_selection import train_test_split
+y = frame["real"].as_matrix()
+x = frame.drop("real", axis=1).as_matrix()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=50, random_state=1)
+
+# На оставшихся 150 настройте два классификатора поддельности банкнот:
+#  1. логистическая регрессия по признакам X1,X2,X3;
+#  2. логистическая регрессия по признакам X4,X5,X6.
+#%%
+from sklearn.linear_model import LogisticRegression
+x_train_1_3 = x_train[np.ix_(range(0, x_train.shape[0]), [0,1,2])]
+x_test_1_3 = x_test[np.ix_(range(0, x_test.shape[0]), [0,1,2])]
+
+x_train_4_6 = x_train[np.ix_(range(0, x_train.shape[0]), [3,4,5])]
+x_test_4_6 = x_test[np.ix_(range(0, x_test.shape[0]), [3,4,5])]
+
+regression_1_3 = LogisticRegression().fit(x_train_1_3, y_train)
+regression_4_6 = LogisticRegression().fit(x_train_4_6, y_train)
+
+# Каждым из классификаторов сделайте предсказания меток классов на тестовой выборке. Одинаковы ли доли ошибочных предсказаний двух классификаторов?
+#%%
+predictions_1_3 = map(lambda x_row: regression_1_3.predict(x_row.reshape(1,-1))[0], x_test_1_3)
+predictions_4_6 = map(lambda x_row: regression_4_6.predict(x_row.reshape(1,-1))[0], x_test_4_6)
+
+print "False predictions portion 1-3: %f" % (1.-float(sum(predictions_1_3))/float(len(predictions_1_3)))
+print "False predictions portion 4-6: %f" % (1.-float(sum(predictions_4_6))/float(len(predictions_4_6)))
+
+# Проверьте гипотезу, вычислите достигаемый уровень значимости. Введите номер первой значащей цифры (например, если вы получили 5.5×10−8, нужно ввести 8).
+#%%
+def proportions_diff_confint_rel(sample1, sample2, alpha = 0.05):
+    z = scipy.stats.norm.ppf(1 - alpha / 2.)
+    sample = zip(sample1, sample2)
+    n = len(sample)
+        
+    f = sum([1 if (x[0] == 1 and x[1] == 0) else 0 for x in sample])
+    g = sum([1 if (x[0] == 0 and x[1] == 1) else 0 for x in sample])
+    
+    left_boundary = float(f - g) / n  - z * np.sqrt(float((f + g)) / n**2 - float((f - g)**2) / n**3)
+    right_boundary = float(f - g) / n  + z * np.sqrt(float((f + g)) / n**2 - float((f - g)**2) / n**3)
+    return (left_boundary, right_boundary)
+
+def proportions_diff_z_stat_rel(sample1, sample2):
+    sample = zip(sample1, sample2)
+    n = len(sample)
+    
+    f = sum([1 if (x[0] == 1 and x[1] == 0) else 0 for x in sample])
+    g = sum([1 if (x[0] == 0 and x[1] == 1) else 0 for x in sample])
+    
+    return float(f - g) / np.sqrt(f + g - float((f - g)**2) / n )
+#%%
+pvalue = proportions_diff_z_test(proportions_diff_z_stat_ind(zev_vector, ctrl_vector))
+print "p-value: %f" % proportions_diff_z_test(proportions_diff_z_stat_rel(predictions_1_3, predictions_4_6))
+
+# В предыдущей задаче посчитайте 95% доверительный интервал для разности долей ошибок двух классификаторов. Чему равна его ближайшая к нулю граница?
+# Округлите до четырёх знаков после десятичной точки.
+#%%
+print "95%% confidence interval for a difference between predictions: [%f, %f]" \
+      % proportions_diff_confint_rel(predictions_1_3, predictions_4_6)
+
+# Ежегодно более 200000 людей по всему миру сдают стандартизированный экзамен GMAT при поступлении на программы MBA.
+# Средний результат составляет 525 баллов, стандартное отклонение — 100 баллов.
+# Сто студентов закончили специальные подготовительные курсы и сдали экзамен. Средний полученный ими балл — 541.4.
+#%%
+n = 200000
+mean = 525
+std = 100
+n_yr = 100
+mean_yr = 541.4
+
+# Проверьте гипотезу о неэффективности программы против односторонней альтернативы о том, что программа работает.
+# Отвергается ли на уровне значимости 0.05 нулевая гипотеза? Введите достигаемый уровень значимости, округлённый до 4 знаков после десятичной точки.
+#%%
+import scipy
+z_stat = scipy.stats.ttest_ind_from_stats(mean, std, n, mean_yr, std, n_yr)
+print(z_stat)
